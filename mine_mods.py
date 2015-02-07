68b55cad2fc2bc7fa6029b9c78851edc6dde2ee0
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2015-02-07 20:20:20 -0200

    doing auxiliar functions

--- mine_mods.py
+++ mine_mods.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import urllib, urllib2
+import urlparse
+
 import yaml
+
+MINECRAFT_PATH = os.environ.get("MINECRAFT_PATH", "/home/arruda/projetos/minecraft-mods-downloader/mc")
+CACHED_PATH = os.path.join(MINECRAFT_PATH, 'cache')
 
 
 def read_mod_list():
@@ -8,16 +15,52 @@ def read_mod_list():
     mod_list = yaml.load(stream)
     return mod_list
 
+
+def get_filename(url):
+    path = urlparse.urlsplit(url).path
+    filename = path.split('/')[-1]
+    return filename
+
+
+def get_mod_url(name, data):
+    for url in data.get('url'):
+
+        try:
+            urllib2.urlopen(url)
+            return url
+        except Exception, e:
+            print(e)
+
+    raise Exception("No url for %s" % name)
+
+
+def download_mod(name, url):
+    filename = get_filename(url)
+    file_path = os.path.join(CACHED_PATH, filename)
+
+    print "saving to: %s" % file_path
+    file_downloader = urllib.URLopener()
+    file_downloader.retrieve(url, file_path)
+    return file_path
+
+
 def install_mods(client=True):
     mod_list = read_mod_list()
 
     for mod, data in mod_list.items():
+        working_url = get_mod_url(mod, data)
+        print "Downloading %(mod)s from %(url)s" % {'mod': mod, 'url': working_url}
+        zip_filepath = download_mod(mod, working_url)
 
+    print "Done!"
 
 
 if __name__ == '__main__':
+    # MINECRAFT_PATH = "/home/arruda/projetos/minecraft-mods-downloader/mc"
 
-    read_mod_list()
+    if not os.path.exists(CACHED_PATH):
+        os.makedirs(CACHED_PATH)
+    install_mods()
     # func_name = sys.argv[1]
     # task = globals()[func_name]
 
