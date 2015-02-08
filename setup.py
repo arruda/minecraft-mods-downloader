47136aa0b41de2807ac0648e3eb8f35a4e930857
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2015-02-08 01:18:50 -0200

    adding requirements and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='minecraft-mods-downloader',
+    version='0.2',
+    description="Control minecraft mods download easily with this tool",
+    py_modules=['mine_mods'],
+    install_requires=[
+        'click>=3.3',
+        'PyYAML>=3.11'
+    ],
+    license="MIT",
+    keywords='minecraft, download, mods, controll',
+    entry_points="""
+        [console_scripts]
+        mine_mods=mine_mods:install_mods
+    """,
+    # test_suite='tests',
+    # tests_require=test_requirements
+)
