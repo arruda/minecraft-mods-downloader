fefeff7611725fc7815b84cd98e69bc4f20b989d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2015-02-08 00:08:13 -0200

    finished \o/ and added requirements =D

--- mine_mods.py
+++ mine_mods.py
@@ -8,10 +8,11 @@ import urllib2
 import urlparse
 import zipfile
 
+import click
 import yaml
 
 
-MINECRAFT_PATH = os.environ.get("MINECRAFT_PATH", "/home/arruda/projetos/minecraft-mods-downloader/mc")
+MINECRAFT_PATH = "."
 CACHED_PATH = os.path.join(MINECRAFT_PATH, 'cache')
 MODS_PATH = os.path.join(MINECRAFT_PATH, 'mods')
 
@@ -32,25 +33,28 @@ def get_mod_working_mirror(name, data):
     for mirror in data.get('mirrors'):
         url = mirror.get('url')
         try:
-            urllib2.urlopen(url)
+            urllib2.urlopen(url, timeout=5)
             return mirror
         except Exception, e:
-            print(e)
+            click.echo(
+                "<Warning>: Mirror with url (%(url)s) not working: %(msg)s" %
+                {'url': url, 'msg': e}
+            )
 
-    raise Exception("No url for %s" % name)
+    raise Exception("No working url for %s mod" % name)
 
 
 def is_mod_cached(cached_filepath):
     return os.path.exists(cached_filepath)
 
 
-def download_mod(name, url):
+def download_mod(name, url, no_cache):
     filename = get_filename(url)
     file_path = os.path.join(CACHED_PATH, filename)
-    if is_mod_cached(file_path):
-        print "Using cache %s" % file_path
+    if is_mod_cached(file_path) and not no_cache:
+        click.echo("Using cache %s" % file_path)
     else:
-        print "Saving to: %s" % file_path
+        click.echo("Downloading to: %s" % file_path)
         file_downloader = urllib.URLopener()
         file_downloader.retrieve(url, file_path)
     return file_path
@@ -58,7 +62,7 @@ def download_mod(name, url):
 
 def unzip_mod_to_mods_path(name, filepath, extract_to):
     file_unzip_path = os.path.join(MINECRAFT_PATH, extract_to)
-    print "Extracting %(mod)s to %(path)s" % {'mod': name, 'path': file_unzip_path}
+    click.echo("Extracting %(mod)s to %(path)s" % {'mod': name, 'path': file_unzip_path})
 
     with zipfile.ZipFile(filepath, "r") as z:
         z.extractall(file_unzip_path)
@@ -67,45 +71,54 @@ def unzip_mod_to_mods_path(name, filepath, extract_to):
 
 
 def copy_mod_to_mods_path(name, filepath):
-    print "Copying %(mod)s to %(path)s" % {'mod': name, 'path': MODS_PATH}
+    click.echo("Copying %(mod)s to %(path)s" % {'mod': name, 'path': MODS_PATH})
     filename = ntpath.basename(filepath)
     dest_path = os.path.join(MODS_PATH, filename)
     shutil.copyfile(filepath, dest_path)
     return dest_path
 
 
-def install_mods(client=True):
-    mod_list = read_mod_list()
-
-    for mod, data in mod_list.items():
-        working_mirror = get_mod_working_mirror(mod, data)
-        download_url = working_mirror.get('url')
-        print "Downloading %(mod)s from %(url)s" % {'mod': mod, 'url': download_url}
-        filepath = download_mod(mod, download_url)
-
-        if working_mirror.get('extract_to', None):
-            unzip_mod_to_mods_path(mod, filepath, working_mirror.get('extract_to'))
-        else:
-            copy_mod_to_mods_path(mod, filepath)
-            pass
-
-        print "Installed %(mod)s version:%(version)s" % {'mod': mod, 'version': data.get('version')}
-
-    print "Done!"
+def set_global_paths(minecraft_path):
+    global MINECRAFT_PATH, CACHED_PATH, MODS_PATH
+    MINECRAFT_PATH = minecraft_path
+    CACHED_PATH = os.path.join(MINECRAFT_PATH, 'cache')
+    MODS_PATH = os.path.join(MINECRAFT_PATH, 'mods')
 
 
-if __name__ == '__main__':
-
+def ensure_paths_exist():
     if not os.path.exists(CACHED_PATH):
         os.makedirs(CACHED_PATH)
 
     if not os.path.exists(MODS_PATH):
         os.makedirs(MODS_PATH)
 
-    install_mods()
-    # func_name = sys.argv[1]
-    # task = globals()[func_name]
 
-    # try:
-    #     task(sys.argv[2:])
-    # except:
+@click.command()
+@click.option('--path',
+              default='.',
+              type=click.Path(exists=True, dir_okay=True, resolve_path=True),
+              help='Path to minecraft instalation directory')
+@click.option('--no-cache', is_flag=True,
+              help='Do not use cache when installing mods')
+def install_mods(path, no_cache):
+    set_global_paths(path)
+    ensure_paths_exist()
+    mod_list = read_mod_list()
+
+    for mod, data in mod_list.items():
+        working_mirror = get_mod_working_mirror(mod, data)
+        download_url = working_mirror.get('url')
+        click.echo("Fetching %(mod)s from %(url)s" % {'mod': mod, 'url': download_url})
+        filepath = download_mod(mod, download_url, no_cache)
+
+        if working_mirror.get('extract_to', None):
+            unzip_mod_to_mods_path(mod, filepath, working_mirror.get('extract_to'))
+        else:
+            copy_mod_to_mods_path(mod, filepath)
+
+        click.echo("Installed %(mod)s version:%(version)s" % {'mod': mod, 'version': data.get('version')})
+
+    click.echo("Done!")
+
+if __name__ == '__main__':
+    install_mods()
